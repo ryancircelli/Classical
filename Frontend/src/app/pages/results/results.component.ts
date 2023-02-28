2cf3a9ee8dc9cf79fae092d6720e6d828e2a1eef
Chandler Carnes <chcarnes12@gmail.com>
2023-02-28 17:10:05 -0500

    added default results data

--- Frontend/src/app/pages/results/results.component.ts
+++ Frontend/src/app/pages/results/results.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+type Person = {
+  upvotes: number;
+  downvotes: number;
+  upvoted: boolean;
+  downvoted: boolean;
+  name: string;
+  dateUpdated: Date;
+};
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent {
+
+  constructor() {}
+
+  results: Person[] = [{
+    upvotes: 4,
+    downvotes: 3,
+    upvoted: false,
+    downvoted: false,
+    name: "CEN3031",
+    dateUpdated: new Date
+  },
+  {
+    upvotes: 2,
+    downvotes: 4,
+    upvoted: true,
+    downvoted: false,
+    name: "CIS4930",
+    dateUpdated: new Date
+  }]
 
 }

2fd798eae62e472613b88a0ecc060c5f0ca2191f
victorpolisetty <52013101+victorpolisetty@users.noreply.github.com>
2023-04-18 16:28:11 -0400

    Updated screenshots and cleaned up code

--- Backend/main.go
+++ Backend/main.go
@@ -20,6 +20,7 @@ func main() {
 	router.HandleFunc("/getClasses", controller.GetClasses).Methods("GET")
 	router.HandleFunc("/createClass", controller.CreateClass).Methods("POST")
 	router.HandleFunc("/deleteClass/{className}", controller.DeleteClass).Methods("DELETE")
+
 	//post API endpoints and functionality
 	router.HandleFunc("/createClassPost", controller.CreateClassPost).Methods("POST")
 	router.HandleFunc("/getPostsByClassName/{className}", controller.GetClassPostsByName).Methods("GET")
@@ -30,19 +31,8 @@ func main() {
 	router.HandleFunc("/getClassesByName/{className}", controller.GetClasessByName).Methods("GET")
 	router.HandleFunc("/getTrendingClasses", controller.GetSortedClasses).Methods("GET")
 
-	// router.HandleFunc("/posts", createPost).Methods("POST")
-	// router.HandleFunc("/posts/{id}", getPost).Methods("GET")
-	// router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
-
 	// Add CORS headers to all responses
 	handler := cors.Default().Handler(router)
 
 	http.ListenAndServe(":8000", handler)
-
-	// posts, err := postsByClassID(1)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("posts found: %v\n", posts)
-
 }
